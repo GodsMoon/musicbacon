9e8b3e2dc6e792cf646b680abde5d7d80ed712bd
David Shellabarger <david@nightshadelabs.com>
2012-05-29 02:24:49 -0400

    trying to get to work

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get('/', routes.index);
 
 
 
-app.listen(3000, function(){
+app.listen(46929, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
