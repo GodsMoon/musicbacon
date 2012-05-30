31d94ab178bc899db4a777a79b684a37fb9fde54
David Shellabarger <david@nightshadelabs.com>
2012-05-30 02:02:04 -0400

    init

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 /**
  * Module dependencies.
  */
-
+var async   = require('async');
+var util = require('util');
 var express = require('express')
   , routes = require('./routes')
   , pg = require('pg').native
@@ -14,28 +15,50 @@ var express = require('express')
 GLOBAL.pg = require('pg').native;
 client = new pg.Client(connectionString);
 client.connect();
-/*query = client.query(
-		'CREATE TABLE taglines				\
-		(									\
-		  id serial NOT NULL,				\
-		  tag character varying(50),		\
-		  likes integer,					\
-		  "user" character varying(50),		\
-		  date date,						\
-		  slug character varying(50),		\
-		  CONSTRAINT id PRIMARY KEY (id ),	\
-		  CONSTRAINT slg UNIQUE (slug )		\
-		)									\
-		WITH (								\
-		  OIDS=FALSE						\
-		);									\
-		ALTER TABLE taglines				\
-		  OWNER TO postgres;');*/
-//query.on('end', function() { client.end(); });
 
-var app = module.exports = express.createServer();
+//var app = module.exports = express.createServer();
 
-GLOBAL.baseurl = "http://127.0.0.1:3000/";
+//create an express webserver
+var app = express.createServer(
+  express.logger(),
+  express.static(__dirname + '/public'),
+  express.bodyParser(),
+  express.cookieParser(),
+  // set this to a secret value to encrypt session cookies
+  express.session({ secret: process.env.SESSION_SECRET || '4a3d7daf2cc5b1646644a9584d8c6c85' }),
+  require('faceplate').middleware({
+    app_id: process.env.FACEBOOK_APP_ID,
+    secret: process.env.FACEBOOK_SECRET,
+    scope:  'user_likes,user_photos,user_photo_video_tags'
+  })
+);
+
+//GLOBAL.baseurl = "http://127.0.0.1:3000/";
+
+
+app.dynamicHelpers({
+	  'host': function(req, res) {
+	    return req.headers['host'];
+	  },
+	  'scheme': function(req, res) {
+	    return req.headers['x-forwarded-proto'] || 'http';
+	  },
+	  'url': function(req, res) {
+	    return function(path) {
+	    	//return 'test';
+	    	url_no_scheme_temp = app.dynamicViewHelpers.url_no_scheme(req, res);
+	      return app.dynamicViewHelpers.scheme(req, res) + url_no_scheme_temp(path);
+	    }
+	  },
+	  'url_no_scheme': function(req, res) {
+	    return function(path) {
+	    	//return 'test';
+	      return '://' + app.dynamicViewHelpers.host(req, res) + path;
+	    }
+	  },
+	});
+
+app.dynamicHelpers({ messages: require('express-messages') });
 
 // Configuration
 
@@ -49,7 +72,7 @@ app.configure(function(){
   require('faceplate').middleware({
     app_id: process.env.FACEBOOK_APP_ID,
     secret: process.env.FACEBOOK_SECRET,
-    scope:  'user_likes,user_photos,user_photo_video_tags'
+    scope:  'user_likes,email,user_location'
   })
   app.use(express.methodOverride());
   app.use(app.router);
@@ -64,12 +87,74 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+function render_page(req, res, next) {
+  req.facebook.app(function(app) {
+    req.facebook.me(function(user) {
+      /*res.render('index', {
+        layout:    false,
+        req:       req,
+        app:       app,
+        user:      user
+      });*/
+
+    	req.app = app;
+    	req.user = user;
+    	next();
+    });
+  });
+}
+
+function handle_facebook_request(req, res, next) {
+	
+  // if the user is logged in
+  if (req.facebook.token) {
+
+    async.parallel([
+      function(cb) {
+        // query 4 friends and send them to the socket for this socket id
+        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
+          req.friends = friends;
+          cb();
+        });
+      },
+      function(cb) {
+        // query 16 photos and send them to the socket for this socket id
+        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
+          req.photos = photos;
+          cb();
+        });
+      },
+      function(cb) {
+        // query 4 likes and send them to the socket for this socket id
+        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
+          req.likes = likes;
+          cb();
+        });
+      },
+      function(cb) {
+        // use fql to get a list of my friends that are using this app
+        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
+          req.friends_using_app = result;
+          cb();
+        });
+      }
+    ], function() {
+      //render_page(req, res, next);
+    });
+
+  } else {
+    //render_page(req, res, next);
+  }
+  next();
+}
+
+
 // Routes
 
-app.post('/post/tagline', routes.post);
-app.get('/shirt/:id', routes.shirt);		
+app.post('/post/tagline',  handle_facebook_request, render_page, routes.post);
+app.get('/shirt/:id', handle_facebook_request, render_page, routes.shirt);		
 
-app.get('/', routes.index);
+app.get('/', handle_facebook_request, render_page, routes.index);
 
 
 var port = process.env.PORT || 3000;
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ exports.index = function(req, res){
 	    client.query("SELECT * FROM taglines", after(function(result) {
 	    	//res.send({ title: 'Music Bacon', tagline: 'hello world', shirts: result });
 
-	    		res.render('index', { title: 'Music Bacon', shirts: result });
+	    		res.render('index', { title: 'Music Bacon', shirts: result, user: req.user });
 	    }))
 	}))
 	//res.send({ title: 'Music Bacon', tagline: 'hello world'});
@@ -24,7 +24,7 @@ exports.shirt = function(req, res){
 		    	client.query("SELECT * FROM taglines where slug='"+req.params.id+"'", after(function(result1) {
 			    	
 		    		         //res.send({ title: 'Music Bacon', tagline: result1.rows[0].tag, shirts: result0, myshirt:result1 });
-		    		res.render('index', { title: 'Music Bacon', tagline: result1.rows[0].tag, shirts: result0, myshirt:result1 })	
+		    		res.render('index', { title: 'Music Bacon', tagline: result1.rows[0].tag, shirts: result0, myshirt:result1, user: req.user })	
 		
 			    }))
 		    }))
@@ -39,14 +39,41 @@ exports.shirt = function(req, res){
 };
 
 exports.post = function(req, res){
+	console.log(req.user);
+	//res.send({ facebook: req.user });
 	
 	if(req.body.tagline){
 		var slug = slugify(req.body.tagline);
-		pg.connect(process.env.DATABASE_URL, after(function(client) {
-			client.query('INSERT INTO taglines(tag,slug) VALUES($1,$2)', [req.body.tagline,slug], after(function(result) {
-				res.redirect('/shirt/'+slug);
+		var safe_text = sanitize(req.body.tagline)
+		
+		console.log("slug:"+slug);
+		console.log("safe_text:"+safe_text);
+		
+		if(slug.length<50 && safe_text.length<50){
+			
+			pg.connect(process.env.DATABASE_URL, after(function(client) {
+				client.query("select name from taglines where slug='"+slug+"'", after(function(exist_test) {
+					console.log("EXISTS TEST:"+exist_test.row[0]);
+					if(exist_test.rows[0])
+						client.query('INSERT INTO taglines(tag,slug) VALUES($1,$2)', [req.body.tagline,slug], after(function(result) {
+							res.redirect('/shirt/'+slug);
+						}))
+					else{
+						//res.send({ ERROR: "There was an error" });
+						console.log("ERROR");
+						req.error = "";
+						req.flash('error', "#{exist_test.rows[0].name} already submitted that tagline<br/>I bet you can come up with something better.");
+						res.redirect('back');
+					}
+						
+				}))
+				
 			}))
-		}))
+		}
+		else{
+			req.flash('error', "Your tagline is too long.");
+			res.redirect('back');
+		}
 	}
 };
 
@@ -58,6 +85,13 @@ var after = function(callback) {
       callback(queryResult)
     }
   };
+  
+function sanitize(text) {
+	text = text.replace(/(\r\n|\n|\r)/gm,"<br/>");
+	text = text.replace(/[^-a-zA-Z0-9</>,&\s]+/ig, '');
+
+	return text;
+};
   
 function slugify(text) {
 	text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
